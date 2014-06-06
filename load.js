8971911a757a24e1a868a019da35411690faf932
Julian <julian@j42.me>
2014-06-06 11:07:54 -0400

    Added root JS loader until integrated with build

---
+++ load.js
@@ -0,0 +1,7 @@
+;(function() {
+	var script = document.createElement('script');
+	script.setAttribute('id','j42-upgradetoday');
+	script.setAttribute('type','text/javascript');
+	script.setAttribute('src','//j42.github.io/F-IE/src/stub.js');
+	document.getElementsByTagName('head')[0].appendChild(script);
+}());
